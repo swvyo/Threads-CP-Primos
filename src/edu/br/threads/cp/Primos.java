b0987f8a4ac2ee061704680d3e1abb2cddb1479e
swvyo <savionascimento14@gmail.com>
2020-11-11 16:12:14 -0300

    Adicionando thread lógica

---
+++ src/edu/br/threads/cp/Primos.java
@@ -0,0 +1,30 @@
+package edu.br.threads.cp;
+
+public class Primos implements Runnable{
+	private int min;
+	private int max;
+
+	public Primos(int x, int y){
+		min = x;
+		max = y;
+ }
+	public boolean prim(int x){
+		for(int i=2; i<x/2+1;i++){
+			if(x%i==0){
+				return false;
+			}
+		}
+	return true;
+ }
+
+ public void run(){
+	 for(int i=min; i<=max;i++){
+		 if(prim(i)){
+			 //exibir os números
+			 //System.out.println(i);
+		 }
+	 }
+	 System.out.println("");
+
+ 	}
+}
